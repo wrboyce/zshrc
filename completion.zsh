04031bf74df4afc27d9e903c86bd7531227939b6
Will Boyce <me@willboyce.com>
2014-04-28 22:50:08 +0100

    pimp zstyle/completion

---
+++ completion.zsh
@@ -0,0 +1,11 @@
+zstyle -e ':completion:*:approximate:*'     max-errors 'reply=( $(( ($#PREFIX + $#SUFFIX) / 3 )) )'
+zstyle ':completion:*:descriptions'         format "- %d -"
+zstyle ':completion:*:corrections'          format "- %d - (errors %e})"
+zstyle ':completion:*:default'              list-prompt '%S%M matches%s'                                    # scroll matches
+zstyle ':completion:*'                      group-name ''                                                   # group all match types
+zstyle ':completion:*'                      verbose yes                                                     # always use verbose form
+
+# Do not complete things beginning with _
+zstyle ':completion:*:functions' ignored-patterns '_*'
+zstyle ':completion:*:parameters' ignored-patterns '_*'
+zstyle ':completion:*:*:*:users' ignored-patterns '_*'
