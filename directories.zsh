3a4f3428266c5366602dbe48fe9b91187c21450b
Will Boyce <me@willboyce.com>
2014-04-29 15:09:25 +0100

    override lib/directories.zsh

--- directories.zsh
+++
@@ -1,4 +0,0 @@
-unsetopt autonamedirs
-
-alias cd='pushd -q'
-alias dc='popd -q'
---
+++ lib/directories.zsh
@@ -0,0 +1,7 @@
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias ..='cd ..'
+alias cd='pushd -q'
+alias dc='popd -q'
