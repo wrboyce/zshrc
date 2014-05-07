2fdcab3bd87086203b50a210605ceb2bcec67cf8
Will Boyce <me@willboyce.com>
2014-05-07 15:39:25 +0100

    add sudo pfctl alias

--- plugins/osx/osx.plugin.zsh
+++ plugins/osx/osx.plugin.zsh
@@ -1,5 +1,6 @@
-alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+alias updatedb='sudo /usr/libexec/locate.updatedb'
+alias pfctl='sudo pfctl'
 
 man-preview () {
     man -t "$@" | open -f -a Preview
