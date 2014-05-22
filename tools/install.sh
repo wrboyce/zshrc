5de0cb402f49fafe280b9a9d338b42fa9c257d0c
Will Boyce <me@willboyce.com>
2014-05-23 00:20:44 +0100

    add install script

---
+++ tools/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -f ~/.zshrc] || [ -d ~/.oh-my-zsh-custom ]; then
+  echo "Previous (oh-my-)zsh config found at ~/.zshrc or ~/.oh-my-zsh-custom! Bailing..." >&2
+  exit
+fi
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "Cloning oh-my-zsh..."
+  /usr/bin/env git clone --recursive https://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
+fi
+echo "Cloning oh-my-zsh-custom..."
+/usr/bin/env git clone --recursive https://github.com/wrboyce/oh-my-zsh-custom.git ${HOME}/.oh-my-zsh/custom
+
+echo "Installing config..."
+ln -s ~/.oh-my-zsh-custom/zshrc ~/.zshrc
