42e88f452b8afcd8f92f3d98ac76dd04c4c8879f
Will Boyce <me@willboyce.com>
2014-10-21 23:00:51 +0100

    add some more apt-get aliases

--- plugins/debian/debian.plugin.zsh
+++ plugins/debian/debian.plugin.zsh
@@ -1,1 +1,5 @@
 alias apt-get='sudo apt-get'
+alias apt-upgrade='apt-get update; apt-get upgrade -y'
+alias apt-dist-upgrade='apt-upgrade; apt-get dist-upgrade -y'
+alias apt-autoremove='apt-get autoremove --purge -y; apt-get autoclean'
+alias apt-clean='apt-autoremove; apt-get clean'
