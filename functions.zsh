e58fa4373b576b66989ccac9154f6203d5121f6a
Will Boyce <me@willboyce.com>
2016-06-21 18:54:38 +0100

    add `get_ip` function

--- functions.zsh
+++ functions.zsh
@@ -5,3 +5,8 @@ function upgrade_zshrc() {
   (cd ${ZSH}; git fetch origin master; git rebase --autostash --stat origin/master)
   (cd ${ZSH_CUSTOM}; git fetch origin master; git rebase --autostash --stat origin/master)
 }
+
+function get_ip() {
+  local interface=$1
+  ifconfig ${interface} | grep 'inet ' | awk '{print $2}'
+}
