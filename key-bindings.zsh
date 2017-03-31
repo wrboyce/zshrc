ad3fd07c613f7c4d21dc2b7b364d3c5d8d72aa32
Will Boyce <me@willboyce.com>
2017-03-31 09:56:55 +0100

    add subshell shortcut (esc-s)

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -1,2 +1,11 @@
 bindkey -s '\el' 'l\n'      # [Esc-l] - run command: l
 bindkey ' ' self-insert     # get rid of magic-space
+
+function _subshell-command-line {
+    if [[ ! -z $BUFFER ]]
+    then
+        LBUFFER="(${BUFFER})"
+    fi
+}
+zle -N _subshell-command-line
+bindkey "\es" _subshell-command-line
