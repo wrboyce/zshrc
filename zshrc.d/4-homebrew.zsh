f83f1120f55f43cf1f485f3184b65724491fbdbd
Will Boyce <me@willboyce.com>
2023-03-01 02:59:10 +0000

    🩹 only load plugins if homebrew is installed

--- zshrc.d/4-homebrew.zsh
+++ zshrc.d/4-homebrew.zsh
@@ -1,13 +1,15 @@
-# cache init script as to avoid binary execution
-brew_cache="${ZSH_HOME}/cache/brew/env.zsh"
-mkdir -p "${brew_cache:h}" 2>/dev/null
-if [[ ! -s "${brew_cache}" || "${commands[brew]}" -nt "${brew_cache}" ]]; then
-    brew shellenv >! "${brew_cache}"
-fi
-zsource "${brew_cache}" false
-zinit ice id-as'homebrew/env' pick'env.zsh' compile'env.zsh'
-zinit load "${brew_cache:h}"
-unset brew_cache
+if (( $+commands[brew] )); then
+    # cache init script as to avoid binary execution
+    brew_cache="${ZSH_HOME}/cache/brew/env.zsh"
+    mkdir -p "${brew_cache:h}" 2>/dev/null
+    if [[ ! -s "${brew_cache}" || "${commands[brew]}" -nt "${brew_cache}" ]]; then
+        brew shellenv >! "${brew_cache}"
+    fi
+    zsource "${brew_cache}" false
+    zinit ice id-as'homebrew/env' pick'env.zsh' compile'env.zsh'
+    zinit load "${brew_cache:h}"
+    unset brew_cache
 
-# load completions
-zinit as=completion id-as=homebrew/completions for "/opt/homebrew/share/zsh/site-functions"
+    # load completions
+    zinit as=completion id-as=homebrew/completions for "/opt/homebrew/share/zsh/site-functions"
+fi
