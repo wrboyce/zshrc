a3c2d42bd9a53e29bac06f28439ad2fb6f03bb24
Will Boyce <me@willboyce.com>
2023-03-01 03:16:35 +0000

    💩 disable gpg-agent plugin

--- zshrc.d/7-gpgagent.zsh
+++ zshrc.d/7-gpgagent.zsh
@@ -1,9 +1,9 @@
-## 
+##
 
 
 if (( ! $+commands[gpg-agent] )) || [ ! -d "${HOME}/.gnupg" ]; then
     return
 fi
 
-zinit ice wait'0' lucid
-zinit snippet OMZ::plugins/gpg-agent
+#zinit ice wait'0' lucid
+#zinit snippet OMZ::plugins/gpg-agent
